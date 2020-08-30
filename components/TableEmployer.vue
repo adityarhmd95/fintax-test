53eb3d4bbe7e6d5c6bfcbedf5c4e1ac6963af8dc
Aditya Rahmad <aditya@lapaktrip.com>
2020-08-31 01:54:19 +0700

    create table and get data from dummy api

--- components/TableEmployer.vue
+++ components/TableEmployer.vue
@@ -1,9 +1,58 @@
 <template>
   <div class="table">
-    <div class="tHead">
+    <div class="tRow tHead">
       <div class="item">Name</div>
-      <div class="item">Salary</div>
       <div class="item">Age</div>
+      <div class="item">Salary</div>
+    </div>
+    <div class="tRow tBody" 
+      v-for="(employer, index) in dataEmployers" 
+      :key="index" 
+      :class="{oddRow : index%2 != 0}">
+      <div class="item">{{employer.employee_name}}</div>
+      <div class="item">{{employer.employee_age}}</div>
+      <div class="item">{{employer.employee_salary}}</div>
     </div>
   </div>
 </template>
+
+<script>
+  export default {
+    props: ['dataEmployers']
+  }
+
+</script>
+
+<style lang="scss" scoped>
+  .table {
+    display: flex;
+    flex-flow: column;
+    max-width: 500px;
+
+    .tRow {
+      display: flex;
+      flex-flow: row;
+      height: 30px;
+      align-items: center;
+
+      &.tHead {
+        background: #3E7DC0;
+        color: #FFFFFF;
+      }
+
+      &.oddRow {
+        background: #e2f0ff;
+      }
+
+      .item {
+        width: 25%;
+        padding: 0 15px;
+
+        &:first-child {
+          width: 50%;
+        }
+      }
+    }
+  }
+
+</style>
--- pages/employer.vue
+++ pages/employer.vue
@@ -1,15 +1,40 @@
 <template>
   <div class="wrapEmployer">
     <h1>List Employer</h1>
-    <TableEmployer />
+    <TableEmployer 
+      v-if="dataEmployers"
+      :dataEmployers="dataEmployers" />
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
   import TableEmployer from '~/components/TableEmployer'
+
   export default {
     components: {
       TableEmployer
+    },
+    data(){
+      return {
+        dataEmployers: null
+      }
+    },
+    created(){
+      this.getEmployerLists()
+    },
+    methods: {
+      getEmployerLists() {
+        axios.get('http://dummy.restapiexample.com/api/v1/employees')
+        .then((response) => {
+          if(response.status === 200) {
+            this.dataEmployers = response.data.data
+          }
+        })
+        .catch(err => {
+          console.error(err)
+        })
+      }
     }
   }
 </script>
