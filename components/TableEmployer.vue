532f8c5582f67d462d256ebb3667da076ea17ac6
Aditya Rahmad <aditya@lapaktrip.com>
2020-08-30 23:41:49 +0700

    setup structure employer

---
+++ components/TableEmployer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="table">
+    <div class="tHead">
+      <div class="item">Name</div>
+      <div class="item">Salary</div>
+      <div class="item">Age</div>
+    </div>
+  </div>
+</template>
---
+++ pages/employer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapEmployer">
+    <h1>List Employer</h1>
+    <TableEmployer />
+  </div>
+</template>
+
+<script>
+  import TableEmployer from '~/components/TableEmployer'
+  export default {
+    components: {
+      TableEmployer
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .wrapEmployer {
+    width: 100%;
+    display: flex;
+    flex-flow: column;
+  }
+</style>
